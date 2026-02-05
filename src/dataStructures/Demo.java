1b8f73deacff9170e3646fb8616dc16f04fc34fa
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-05 19:52:42 +0530

    Added binary search to previous

--- src/dataStructures/Demo.java
+++ src/dataStructures/Demo.java
@@ -11,12 +11,33 @@ public class Demo {
 		return -1;
 	}
 	
+	public static int binarySearch(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1;
+		
+		while(left <= right) {
+			int mid = (left + right)/2;
+			
+			if(nums[mid] == target) {
+				return mid;
+			}
+			else if(nums[mid] < target) {
+				left = mid + 1;
+			}
+			else {
+				right  = mid - 1;
+			}
+		}
+		return -1;
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		int[] nums = {1,2,4,6,7};
 		int target = 6;
 		
-		int result = linearSearch(nums, target);
+		int result = binarySearch(nums, target);
 		
 		if(result != target) {
 			System.out.println("Target found at index : " + result);
