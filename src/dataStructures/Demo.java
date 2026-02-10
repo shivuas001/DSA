57198f559264b41145395ded443a246c71ffba2e
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-10 21:31:05 +0530

    steps taken by linear and binary search

--- src/dataStructures/Demo.java
+++ src/dataStructures/Demo.java
@@ -3,22 +3,29 @@ package dataStructures;
 public class Demo {
 	
 	public static int linearSearch(int[] nums, int target) {
+		int steps = 0;
 		for(int i=0;i<nums.length;i++) {
+			steps++;
 			if(nums[i] == target) {
+				System.out.println("steps taken by linearSearch to find the target:" + steps);
 				return i;
 			}
 		}
+		System.out.println("steps taken by linearSearch to find the target:" + steps);
 		return -1;
 	}
 	
 	public static int binarySearch(int[] nums, int target) {
 		int left = 0;
 		int right = nums.length - 1;
+		int steps = 0;
 		
 		while(left <= right) {
+			steps++;
 			int mid = (left + right)/2;
 			
 			if(nums[mid] == target) {
+				System.out.println("steps taken by binarySearch to find the target:" + steps);
 				return mid;
 			}
 			else if(nums[mid] < target) {
@@ -28,6 +35,7 @@ public class Demo {
 				right  = mid - 1;
 			}
 		}
+		System.out.println("steps taken by binarySearch to find the target:" + steps);
 		return -1;
 		
 	}
@@ -35,16 +43,11 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		int[] nums = {1,2,4,6,7};
-		int target = 6;
+		int target = 7;
 		
-		int result = binarySearch(nums, target);
+		int result1 = linearSearch(nums, target);
+		int result2 = binarySearch(nums, target);
 		
-		if(result != target) {
-			System.out.println("Target found at index : " + result);
-		}
-		else {
-			System.out.println("Target not found");
-		}
 		
 	}
 
