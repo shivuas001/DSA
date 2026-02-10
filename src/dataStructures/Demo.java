a2dda36682889695a9c72c02a23be1a3de9cc47e
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-10 22:35:37 +0530

    Recursion for BinarySeach

--- src/dataStructures/Demo.java
+++ src/dataStructures/Demo.java
@@ -1,54 +1,36 @@
 package dataStructures;
 
+//BinarySearch using Recursive funtion.
 public class Demo {
 	
-	public static int linearSearch(int[] nums, int target) {
-		int steps = 0;
-		for(int i=0;i<nums.length;i++) {
-			steps++;
-			if(nums[i] == target) {
-				System.out.println("steps taken by linearSearch to find the target:" + steps);
-				return i;
-			}
-		}
-		System.out.println("steps taken by linearSearch to find the target:" + steps);
-		return -1;
-	}
-	
-	public static int binarySearch(int[] nums, int target) {
-		int left = 0;
-		int right = nums.length - 1;
-		int steps = 0;
+	public static int binarySearch(int[] nums, int target, int left, int right) {
 		
-		while(left <= right) {
-			steps++;
+	//no need of loop as it is recursive we calling method again and again 
+		if(left <= right) {
 			int mid = (left + right)/2;
 			
 			if(nums[mid] == target) {
-				System.out.println("steps taken by binarySearch to find the target:" + steps);
 				return mid;
 			}
 			else if(nums[mid] < target) {
-				left = mid + 1;
+				return binarySearch(nums, target, left=mid+1, right); //calling method again and again
 			}
 			else {
-				right  = mid - 1;
+				return binarySearch(nums, target, left, right=mid-1);
 			}
 		}
-		System.out.println("steps taken by binarySearch to find the target:" + steps);
 		return -1;
 		
 	}
 	
 	public static void main(String[] args) {
-	
-	//lets not specify values, will take an array that have 1000 values in it. linear takes 1000 steps and binary takes 10 steps.
-		int[] nums = new int[1000];
-		int target = 7;
 		
-		int result1 = linearSearch(nums, target);
-		int result2 = binarySearch(nums, target);
+		int[] nums = {3, 5, 7, 9, 11};
+		int target = 9;
 		
+		int result = binarySearch(nums, target, 0, nums.length-1);
+		
+		System.out.println(result);
 		
 	}
 
