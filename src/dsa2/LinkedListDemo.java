f246f65dd1cf55d579948a4230035a74f84c6f1a
A SHIVA KUMAR <shivuas605@gmail.com>
2026-02-19 19:57:07 +0530

    LinkedList - creating a new node

---
+++ src/dsa2/LinkedListDemo.java
@@ -0,0 +1,35 @@
+package dsa2;
+
+class Node{
+	int data;
+	Node next;
+	
+	public Node(int data) 
+	{
+		this.data = data;
+		next = null;
+	}
+}
+
+
+class LinkedList{
+	
+	public int add(int data)
+	{
+		Node newNode = new Node(data);
+		
+		return data;
+	}
+}
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+        LinkedList nums = new LinkedList();
+ 
+        
+      
+	}
+
+}
